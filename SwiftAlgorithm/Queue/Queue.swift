1143b1e4370b1832af876fa32536a32d2e87f85d
wonhui-kim <0512tini@gmail.com>
2023-02-09 10:51:26 +0900

    큐 구현 - 효율성 증가 head 사용

--- SwiftAlgorithm/Queue/Queue.swift
+++ SwiftAlgorithm/Queue/Queue.swift
@@ -10,34 +10,34 @@ import Foundation
 struct Queue<T> {
     private var queue = [T?]() //nil 할당 위해
     private var head: Int = 0
-    
+
     public mutating func enqueue(_ element: T) {
         queue.append(element)
     }
-    
+
     public mutating func dequeue() -> T? {
         guard head <= queue.count, let element = queue[head] else {
             return nil
         }
         queue[head] = nil
         head += 1
-        
+
         if head > 50 {
             queue.removeFirst(head)
             head = 0
         }
-        
+
         return element
     }
-    
+
     public func peek() -> T? {
         return isEmpty ? nil : queue[head]
     }
-    
+
     public var count: Int {
         return queue.count
     }
-    
+
     public var isEmpty: Bool {
         return queue.isEmpty
     }
