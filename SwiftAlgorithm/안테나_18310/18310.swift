ebdfba31ee07a74d93e0b0947846e8a89ba41654
wonhui-kim <0512tini@gmail.com>
2023-03-28 13:20:28 +0900

    백준 18310 안테나 - sorting

---
+++ SwiftAlgorithm/안테나_18310/18310.swift
@@ -0,0 +1,70 @@
+//
+//  18310.swift
+//  SwiftAlgorithm
+//
+//  Created by kim-wonhui on 2023/03/27.
+//
+import Foundation
+
+final class FileIO {
+    private let buffer: Data
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        self.buffer = try! fileHandle.readToEnd()! // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer {
+            index += 1
+        }
+        guard index < buffer.count else { return 0 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+
+        return str
+    }
+}
+
+let FIO = FileIO()
+
+let N = FIO.readInt()
+
+var houses = [Int]()
+for _ in 0..<N {
+    houses.append(FIO.readInt())
+}
+houses.sort()
+
+print(houses[(houses.count-1)/2])
