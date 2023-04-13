ea55bb32368d174050074bbeb02aeb66117e4bd1
wonhui-kim <0512tini@gmail.com>
2023-04-13 10:49:04 +0900

    프로그래머스 문자열다루기 - Int(String) 시, String이 모두 숫자로 이뤄져있지 않다면 nil

--- SwiftAlgorithm/문자열다루기기본'/문자열다루기기본.swift
+++ SwiftAlgorithm/문자열다루기기본'/문자열다루기기본.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 func solution(_ s:String) -> Bool {
-    return Int(s) != nil && s.count == 4 || s.count == 6 ? true : false
+    return Int(s) != nil && (s.count == 4 || s.count == 6) ? true : false
 }
